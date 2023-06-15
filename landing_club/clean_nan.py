0511f0e1be5414919baf772443cd35ca3e5b4f0e
Heiko Vogeler <heiko.vogeler@hvo.de>
2023-06-15 09:16:35 +0000

    dynamically determine pickel directory in sandbox

--- landing_club/clean_nan.py
+++
@@ -1,9 +0,0 @@
-
-# Remove records with nan
-acc_df = acc_raw_df.loc[:, col_names]
-#             .query('loan_amnt != 0')
-
-acc_df = acc_df.dropna()
-print(acc_df.sample(10))
-acc_df.describe()
-acc_df.int_rate.describe()
--- landing_club/landing_club_pickle_wrapper.py
+++ landing_club/landing_club_pickle_wrapper.py
@@ -1,17 +1,21 @@
+'''
+This wrapper is called as external package from a graph transform function
+Needs to be provided with the model
+'''
 import pickle as p
+import os
 from landing_club_wrapper import LandingClubWrapper
 
+project_dir = os.getenv('PROJECT_DIR')
 wrapper_file = open(
-    '/disk1/dec/decisioning/py/landing_club_wrapper.pickle', 'rb')
+    project_dir + '/py/landing_club_wrapper.pickle', 'rb')
 wrapper: LandingClubWrapper = p.load(wrapper_file)
-
 
 def preprocess(loan_amnt, home_ownership, annual_inc):
     '''Because Ab Initio external packages can not call object methods we need
        to proxy it
     '''
     return wrapper.preprocess(loan_amnt, home_ownership, annual_inc)
-
 
 def postprocess(predict):
     '''Because Ab Initio external packages can not call object methods we need
